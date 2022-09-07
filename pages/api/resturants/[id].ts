e7a458159fa68fdbd46179840a8154e9f33c6569
Jesse Anderson <jesse.c.anderson@icloud.com>
2022-09-07 15:58:31 -0400

    restaurant work is almost done, realizing I need to fix the names on the files also

--- pages/api/resturants/[id].ts
+++ pages/api/resturants/[id].ts
@@ -10,17 +10,55 @@ export default async function handler(
     method,
   } = req;
 
-  console.log(`Id for request is: ${id}`);
+  switch (req.method) {
+    // Get current restaurant by ID
+    case "GET":
+      try {
+        const result = await getRequest(`${id}`);
+        res.status(200).json({ response: result.rows });
+      } catch (error) {
+        res.status(500).json({ response: `${error}` });
+      }
+    // Create new Restaurant with ID
+    case "POST":
+      try {
+        const result = await postRequset(`${id}`, `${name}`);
+        res.status(200).json({
+          response: {
+            restaurant: result.fields,
+          },
+        });
+      } catch (error) {
+        res.status(500).json({ response: `${error}` });
+      }
+  }
 
+  //   res.status(200).json({ response: "success" });
+}
+
+async function getRequest(id: string) {
   try {
     const client = await pool.connect();
-    const query = `select * from resturants where id=${id};`;
+    const query = `select * from "RestaurantSchema".restaurant where id=${id};`;
     const result = await client.query(query);
     console.log(result);
     client.release();
+    return result;
   } catch (error) {
     console.log(error);
-    res.status(500).json({ response: `${error}` });
+    throw error;
   }
-  res.status(200).json({ response: "success" });
+}
+
+async function postRequset(id: string, name: string) {
+  try {
+    const client = await pool.connect();
+    const decodedName = decodeURI(name);
+    const query = `INSERT INTO "RestaurantSchema".restaurant (id, name) VALUES ('${id}'::integer, '${decodedName}') returning id;`;
+    const result = await client.query(query);
+    client.release();
+    return result;
+  } catch (error) {
+    throw error;
+  }
 }
--- pages/api/resturants/index.ts
+++ pages/api/resturants/index.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import pool from "../../../lib/db";
 
 export default async function handler(
   req: NextApiRequest,
@@ -7,15 +8,30 @@ export default async function handler(
   const query = req.query;
   const { lat, lng } = query;
 
-  const apiKey = process.env.GOOGLE_API_KEY;
+  try {
+    const client = await pool.connect();
+    const query = 'select * from "RestaurantSchema".restaurant;';
+    const result = await client.query(query);
+    client.release();
+    res.status(200).json({
+      response: {
+        count: result.rowCount,
+        restaurants: result.rows,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ response: `${error}` });
+  }
 
-  const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants&location=${lat},${lng}&radius=2000&region=us&type=restaurant&key=${apiKey}`;
-  const headers = { "Content-Type": "application/json" };
+  // const apiKey = process.env.GOOGLE_API_KEY;
 
-  const response = await fetch(url, {
-    headers: headers,
-  });
-  const data = await response.json();
+  // const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants&location=${lat},${lng}&radius=2000&region=us&type=restaurant&key=${apiKey}`;
+  // const headers = { "Content-Type": "application/json" };
 
-  res.status(200).json(data.results);
+  // const response = await fetch(url, {
+  //   headers: headers,
+  // });
+  // const data = await response.json();
+
+  // res.status(200).json(data.results);
 }
