47d7e86f11d1eb7191fed0141c1b370bea3b3d0c
Jesse Anderson <jesse.c.anderson@icloud.com>
2022-09-08 11:57:00 -0400

    login working

--- pages/account_page.tsx
+++ pages/account_page.tsx
@@ -16,8 +16,15 @@ const LoginPage: NextPage = () => {
   }
   return (
     <Layout>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-24 px-14 text-center">
+        Not signed in <br />
+        <button
+          className="inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-transparent hover:text-white hover:bg-teal-500 mt-4 lg:mt-0"
+          onClick={() => signIn()}
+        >
+          Sign in
+        </button>
+      </div>
     </Layout>
   );
 };
