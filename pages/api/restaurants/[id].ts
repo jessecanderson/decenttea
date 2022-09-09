4ceb606e2911936ccc010f9d5d06d1351b2ffbaa
Jesse Anderson <jesse.c.anderson@icloud.com>
2022-09-08 23:36:02 -0400

    forgot to save a file

--- pages/api/restaurants/[id].ts
+++ pages/api/restaurants/[id].ts
@@ -19,23 +19,6 @@ export default async function handler(
       } catch (error) {
         res.status(500).json({ response: `${error}` });
       }
-    // Create new Restaurant with ID
-    case "POST":
-      try {
-        const result = await postRequset(
-          `${id}`,
-          `${name}`,
-          `${lat}`,
-          `${lng}`
-        );
-        res.status(200).json({
-          response: {
-            restaurant: result.fields,
-          },
-        });
-      } catch (error) {
-        res.status(500).json({ response: `${error}` });
-      }
   }
 
   //   res.status(200).json({ response: "success" });
@@ -54,25 +37,6 @@ async function getRequest(id: number) {
     return result;
   } catch (error) {
     console.log(error);
-    throw error;
-  }
-}
-
-async function postRequset(id: string, name: string, lat: string, lng: string) {
-  try {
-    const client = new PrismaClient();
-    const decodedName = decodeURI(name);
-
-    console.log(`${lat}, ${lng}`);
-    const result = await client.restaurants.create({
-      data: {
-        id: Number(id),
-        name: decodedName,
-      },
-    });
-    client.$disconnect;
-    return result;
-  } catch (error) {
     throw error;
   }
 }
