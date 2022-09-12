b8b200bc72c095c4598aaf29b7f212c2a26f88cc
Jesse Anderson <jesse.c.anderson@icloud.com>
2022-09-12 16:57:30 -0400

    return lists by idea

--- pages/api/lists/[id].ts
+++ pages/api/lists/[id].ts
@@ -5,18 +5,60 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { id, userId } = req.query;
+  const { id, restaurantId } = req.query;
 
-  console.log(`list id = ${id}, user id = ${userId}`);
+  console.log(`list id = ${id}, user id = ${restaurantId}`);
 
   switch (req.method) {
     case "GET":
-      res.status(200).json({ response: "success" });
+      const lists = await getListById();
+      res.status(200).json({ response: lists });
       break;
     case "PUT":
-      res.status(200).json({ response: "adding to list" });
+      const result = await addToList();
+      res.status(200).json({ response: result });
       break;
     default:
       res.status(405).json({ response: "Method not allowed" });
+  }
+
+  async function getListById() {
+    try {
+      const client = new PrismaClient();
+      const result = await client.list.findUnique({
+        where: {
+          id: Number(id),
+        },
+        include: {
+          restaurants: true,
+        },
+      });
+      client.$disconnect();
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function addToList() {
+    try {
+      const client = new PrismaClient();
+      const result = await client.list.update({
+        where: {
+          id: Number(id),
+        },
+        data: {
+          restaurants: {
+            connect: {
+              id: Number(restaurantId),
+            },
+          },
+        },
+      });
+      client.$disconnect();
+      return result;
+    } catch (error) {
+      throw error;
+    }
   }
 }
