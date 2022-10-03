902fb2ac86e6fa7aaef9a3b2513cd7353f2ded19
Jesse Anderson <jesse.c.anderson@icloud.com>
2022-10-03 09:06:34 -0400

    moved card to new component and updated tea search and add restaurant components to use it

--- components/add_restaurant.tsx
+++ components/add_restaurant.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 
 import React, { useEffect, useState } from "react";
+import Card from "./card";
 
 interface Props {
   callback: () => void;
@@ -13,26 +14,24 @@ const AddRestaurant: NextPage<Props> = (props) => {
   };
 
   return (
-    <div className="container">
-      <div className="w-full rounded-xl shadow p-4">
-        <h2 className="font-bold py-2">Add a Restaurant</h2>
-        <p>Not able to find your favorite tea place? Add it here!</p>
+    <Card>
+      <h2 className="font-bold py-2">Add a Restaurant</h2>
+      <p>Not able to find your favorite tea place? Add it here!</p>
 
-        <div className="mb-4 grid grid-flow-col gap-4">
-          <form onSubmit={handleSubmit}>
-            <div className="grid grid-cols-2 gap-2">
-              <input
-                className="cursor-pointer"
-                type="button"
-                value="Cancel"
-                onClick={props.callback}
-              />
-              <input className="cursor-pointer" type="submit" value="Save" />
-            </div>
-          </form>
-        </div>
+      <div className="mb-4 grid grid-flow-col gap-4">
+        <form onSubmit={handleSubmit}>
+          <div className="grid grid-cols-2 gap-2">
+            <input
+              className="cursor-pointer"
+              type="button"
+              value="Cancel"
+              onClick={props.callback}
+            />
+            <input className="cursor-pointer" type="submit" value="Save" />
+          </div>
+        </form>
       </div>
-    </div>
+    </Card>
   );
 };
 
--- components/tea_search.tsx
+++ components/tea_search.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import useSWR from "swr";
 import React, { useState, useEffect } from "react";
 import { Address, Position } from "../global/types";
+import Card from "./card";
 
 var startingAddress: Address = {
   streetOne: "",
@@ -77,74 +78,68 @@ const TeaSearch: NextPage<Props> = (props) => {
   };
 
   return (
-    <div className="container">
-      <div className="w-full rounded-xl shadow p-4">
-        <h2 className="font-bold py-2">Search for tea near you.</h2>
-        <div className="mb-4 grid grid-flow-col gap-4">
-          <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+    <Card>
+      <h2 className="font-bold py-2">Search for tea near you.</h2>
+      <div className="mb-4 grid grid-flow-col gap-4">
+        <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+          <div className="flex flex-col">
+            <label>Street Address 1</label>
+            <input
+              type="text"
+              name="streetOne"
+              value={address.streetOne}
+              onChange={handleAddressInputChange}
+              className="w-full max-w-lg px-2 border-2 rounded"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label>Street Address 2</label>
+            <input
+              type="text"
+              name="streetTwo"
+              value={address.streetTwo}
+              onChange={handleAddressInputChange}
+              className="w-full max-w-lg px-2 border-2 rounded"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-end content-center gap-2">
             <div className="flex flex-col">
-              <label>Street Address 1</label>
+              <label>City</label>
               <input
                 type="text"
-                name="streetOne"
-                value={address.streetOne}
+                name="city"
+                value={address.city}
                 onChange={handleAddressInputChange}
-                className="w-full max-w-lg px-2 border-2 rounded"
+                className="px-2 border-2 rounded"
               />
             </div>
             <div className="flex flex-col">
-              <label>Street Address 2</label>
+              <label>State</label>
               <input
                 type="text"
-                name="streetTwo"
-                value={address.streetTwo}
+                name="state"
+                value={address.state}
                 onChange={handleAddressInputChange}
-                className="w-full max-w-lg px-2 border-2 rounded"
+                className="px-2 border-2 rounded"
               />
             </div>
-            <div className="grid grid-cols-4 content-center gap-2">
-              <div className="flex flex-col">
-                <label>City</label>
-                <input
-                  type="text"
-                  name="city"
-                  value={address.city}
-                  onChange={handleAddressInputChange}
-                  className="px-2 border-2 rounded"
-                />
-              </div>
-              <div className="flex flex-col">
-                <label>State</label>
-                <input
-                  type="text"
-                  name="state"
-                  value={address.state}
-                  onChange={handleAddressInputChange}
-                  className="px-2 border-2 rounded"
-                />
-              </div>
-              <div className="flex flex-col">
-                <label>Zip</label>
-                <input
-                  type="text"
-                  name="zip"
-                  value={`${address.zip}`}
-                  onChange={handleAddressInputChange}
-                  className="px-2 border-2 rounded"
-                />
-              </div>
-              <div className="flex justify-center border rounded-md m-4 p-2 cursor-pointer">
-                <input
-                  className="cursor-pointer"
-                  type="submit"
-                  value="Search"
-                />
-              </div>
+            <div className="flex flex-col">
+              <label>Zip</label>
+              <input
+                type="text"
+                name="zip"
+                value={`${address.zip}`}
+                onChange={handleAddressInputChange}
+                className="px-2 border-2 rounded"
+              />
             </div>
-          </form>
-        </div>
+            <div className="w-full border-2 rounded-md p-2 text-center cursor-pointer hover:text-white hover:bg-teal-500">
+              <input className="cursor-pointer" type="submit" value="Search" />
+            </div>
+          </div>
+        </form>
       </div>
-    </div>
+    </Card>
   );
 };
 
